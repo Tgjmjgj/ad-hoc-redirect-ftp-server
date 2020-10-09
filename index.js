21d6e2694fc4180362e11297f221b50da8f48300
Pavel <Chesh397@mail.ru>
2020-10-09 23:25:36 +0300

    Require explicit external ip address

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const GenerativeFS = require('./generativeFS/fs');
 const fs = new GenerativeFS();
 
 const port = process.env.PORT || '3855';
+const externalIP = process.env.EXTERNAL_IP || '0.0.0.0';
 
 const FtpSrv = require('ftp-srv');
 const ftpServer = new FtpSrv({
-    url: 'ftp://192.168.32.130:' + port,
+    url: `ftp://${externalIP}:${port}`,
     anonymous: true,
-    pasv_url: 'ftp://192.168.32.130',
+    pasv_url: `ftp://${externalIP}`,
     logLevel: 'trace',
 });
 
